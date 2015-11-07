9ef2f4147298e26bb91219368f1878bcf638d3b8
johnleverses <johncle@gmail.com>
2015-11-07 16:09:06 +0000

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Profile::Application.routes.draw do
+  root 'profiles#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
