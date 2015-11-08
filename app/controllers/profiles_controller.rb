d00ed7cefbd7fa24a4a9bbb923b9eec55d00fa12
johnleverses <johncle@gmail.com>
2015-11-08 19:17:15 +0000

    Building a form to save new quotes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,9 @@ class ProfilesController < ApplicationController
 	def index
   		@whoisme = Whoisme.order("RANDOM()").first
 	end
+
+	def new
+		@whoisme = Whoisme.new
+  	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Profile::Application.routes.draw do
   root 'profiles#index'
+
+  resources :profiles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
